097030b54b6ee7aae3e85fb40983376b2a19342c
Alexander Petrushin <stuartrevol@gmail.com>
2022-03-13 22:24:52 +0300

    Initial commit

---
+++ nodes_count_enum.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class NodesCountEnum(Enum):
+    FIFTEEN_NODES = 15
+    TWENTY_ONE_NODES = 21
+    THIRTY_ONE_NODES = 31
+    FORTY_ONE_NODES = 41
+    FIFTY_ONE_NODES = 51
+    SIXTY_ONE_NODES = 61
