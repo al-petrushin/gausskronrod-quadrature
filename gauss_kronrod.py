5d0bb99741e4945bf9208c14d3975c258f628526
Alexander Petrushin <stuartrevol@gmail.com>
2022-03-13 22:28:41 +0300

    Multiprocessing version

--- gauss_kronrod.py
+++ gauss_kronrod.py
@@ -1,39 +1,94 @@
 from consts import gauss_weights, kronrod_weights, gauss_kronrod_nodes
-from nodes_count_enum import NodesCountEnum
-from scipy.integrate import quad
-from math import cos, sin, sqrt
+from math import sqrt
 from bisect import insort
 import numpy as np
-from datetime import datetime
-import time
+import multiprocessing as mp
+import psutil
 
 
-def get_current_kronrod_weight(nodes_count):
-    global available_kronrod_weights
+class GaussKronrodQuadrature:
+    def __init__(self):
+        self.KRONROD_WEIGHT = []
+        self.GAUSS_WEIGHT = []
+        self.GAUSS_KRONROD_NODES = []
 
-    if len(available_kronrod_weights) == 0:
-        available_kronrod_weights = np.append(kronrod_weights[nodes_count], kronrod_weights[nodes_count][:-1][::-1])
+    def set_nodes(self, nodes):
+        self.KRONROD_WEIGHT = self.get_current_kronrod_weight(nodes)
+        self.GAUSS_WEIGHT = self.get_current_gauss_weight(nodes)
+        self.GAUSS_KRONROD_NODES = self.get_current_gauss_kronrod_nodes(nodes)
 
-    return available_kronrod_weights
+    def integrate(self, f, a, b, args=(), min_intervals=12, limit=200, tol=1e-10):
+        fv = np.vectorize(f)
+        intervals = []
 
+        limits = np.linspace(a, b, min_intervals + 1)
 
-def get_current_gauss_kronrod_nodes(nodes_count):
-    global available_gauss_kronrod_nodes
+        with mp.Pool(psutil.cpu_count(logical=False)) as first_pool:
+            results = [
+                first_pool.apply(
+                    self.integrate_gausskronrod,
+                    args=[fv, left, right, args]
+                ) for left, right in zip(limits[:-1], limits[1:])
+            ]
 
-    if len(available_gauss_kronrod_nodes) == 0:
-        available_gauss_kronrod_nodes = np.append(
+        for I, err, left, right in results:
+            insort(intervals, (err, left, right, I))
+
+        while True:
+            Itotal = sum([x[3] for x in intervals])
+            err2 = sum([x[0] ** 2 for x in intervals])
+            err = sqrt(err2)
+
+            if abs(err / Itotal) < tol:
+                return Itotal, err
+
+            # нет сходимости
+            if len(intervals) >= limit:
+                return False
+
+            err, left, right, I = intervals.pop()
+
+            # разделяем интеграл
+            mid = left + (right - left) / 2
+
+            # вычисляем интегралы и ошибки, заменяем один элемент в списке и добавляем другой в конец
+            I, err, a, b = self.integrate_gausskronrod(fv, left, mid, args)
+            insort(intervals, (err, left, mid, I))
+            I, err, a, b = self.integrate_gausskronrod(fv, mid, right, args)
+            insort(intervals, (err, mid, right, I))
+
+    def integrate_gausskronrod(self, f, a, b, args=()):
+        assert b > a
+
+        mid = 0.5 * (b + a)
+        dx = 0.5 * (b - a)
+        zi = mid + self.GAUSS_KRONROD_NODES * dx
+
+        integrand = f(zi)
+
+        integral_G = np.sum(integrand * self.GAUSS_WEIGHT)
+        integral_K = np.sum(integrand * self.KRONROD_WEIGHT)
+
+        error = (200 * abs(integral_G - integral_K)) ** 1.5
+
+        return integral_K * dx, dx * error, a, b
+
+    @classmethod
+    def get_current_kronrod_weight(cls, nodes_count):
+        return np.append(kronrod_weights[nodes_count], kronrod_weights[nodes_count][:-1][::-1])
+
+    @classmethod
+    def get_current_gauss_kronrod_nodes(cls, nodes_count):
+        return np.append(
             gauss_kronrod_nodes[nodes_count],
             np.negative(gauss_kronrod_nodes[nodes_count][:-1][::-1])
         )
 
-    return available_gauss_kronrod_nodes
+    @classmethod
+    def get_current_gauss_weight(cls, nodes_count):
+        changedGaussWeightArr = gauss_weights[nodes_count][:-1][::-1] \
+            if divmod(nodes_count, 10)[0] % 2 else gauss_weights[nodes_count][::-1]
 
-
-def get_current_gauss_weight(nodes_count):
-    global available_gauss_weights
-
-    if len(available_gauss_weights) == 0:
-        changedGaussWeightArr = gauss_weights[nodes_count][:-1][::-1] if divmod(nodes_count, 10)[0] % 2 else gauss_weights[nodes_count][::-1]
         changedGaussWeightArr = np.append(gauss_weights[nodes_count], changedGaussWeightArr)
 
         for item in range(len(changedGaussWeightArr)):
@@ -41,76 +96,4 @@ def get_current_gauss_weight(nodes_count):
 
             changedGaussWeightArr = np.concatenate((changedGaussWeightArr[:index], [0], changedGaussWeightArr[index:]))
 
-        available_gauss_weights = np.append([0], changedGaussWeightArr)
-
-    return available_gauss_weights
-
-
-def integrate_gausskronrod(f, a, b, nodes, args=()):
-    assert b > a
-
-    mid = 0.5 * (b + a)
-    dx = 0.5 * (b - a)
-    zi = mid + get_current_gauss_kronrod_nodes(nodes) * dx
-
-    integrand = f(zi)
-
-    integral_G = np.sum(integrand * get_current_gauss_weight(nodes))
-    integral_K = np.sum(integrand * get_current_kronrod_weight(nodes))
-
-    error = (200 * abs(integral_G - integral_K)) ** 1.5
-
-    return integral_K * dx, dx * error
-
-
-def integrate(f, a, b, nodes, args=(), min_intervals=1, limit=200, tol=1e-10):
-    fv = np.vectorize(f)
-
-    intervals = []
-
-    limits = np.linspace(a, b, min_intervals + 1)
-
-    for left, right in zip(limits[:-1], limits[1:]):
-        I, err = integrate_gausskronrod(fv, left, right, nodes, args)
-        insort(intervals, (err, left, right, I))
-
-    while True:
-        Itotal = sum([x[3] for x in intervals])
-        err2 = sum([x[0] ** 2 for x in intervals])
-        err = sqrt(err2)
-
-        if abs(err / Itotal) < tol:
-            return Itotal, err
-
-        # нет сходимости
-        if len(intervals) >= limit:
-            return False
-
-        err, left, right, I = intervals.pop()
-
-        # разделяем интеграл
-        mid = left + (right - left) / 2
-
-        # вычисляем интегралы и ошибки, заменяем один элемент в списке и добавляем другой в конец
-        I, err = integrate_gausskronrod(fv, left, mid, nodes, args)
-        insort(intervals, (err, left, mid, I))
-        I, err = integrate_gausskronrod(fv, mid, right, nodes, args)
-        insort(intervals, (err, mid, right, I))
-
-
-if __name__ == "__main__":
-    available_kronrod_weights, available_gauss_weights, available_gauss_kronrod_nodes = [], [], []
-    p = 100
-    f = lambda x: x * sin(p * x)
-    g = lambda x: -x / p * cos(p * x) + 1 / p ** 2 * sin(p * x)
-    a, b = 1, 4
-    nodes = NodesCountEnum.FIFTEEN_NODES.value
-
-    start_time = datetime.now()
-    expected = g(b) - g(a)
-
-    for result, esterror in (quad(f, a, b), integrate(f, a, b, nodes)):
-        print("{:15.13f} {:15g} {:15g}".format(result, esterror, 1 - result / expected))
-
-    time.sleep(5)
-    print('TIME:', datetime.now() - start_time)
+        return np.append([0], changedGaussWeightArr)
